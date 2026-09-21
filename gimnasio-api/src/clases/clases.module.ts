f7557b227c661fd6de3138a0ee665154e37b18ed
rodvillarg <jesus.villegas261186@potros.itson.edu.mx>
2026-09-20 21:49:29 -0700

    Delete gimnasio-api directory

--- gimnasio-api/src/clases/clases.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ClasesController } from './clases.controller.js';
-import { ClasesService } from './clases.service.js';
-
-@Module({
-    controllers: [ClasesController],
-    providers: [ClasesService]
-})
-export class ClasesModule {}
